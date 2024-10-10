ba783bfcc790f969e00433b3e878a7dd90b10f3b
yu7dai <yudaiokuda0109@gmail.com>
2024-10-10 14:58:15 +0900

    掛け算機能を追加

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
+// 足し算関数
 function calculateSum() {
-    // 入力値を取得
     const num1 = parseFloat(document.getElementById("num1").value);
     const num2 = parseFloat(document.getElementById("num2").value);
-    
-    // 足し算を実行
     const sum = num1 + num2;
-    
-    // 結果を表示
     document.getElementById("result").textContent = sum;
+}
+
+// 掛け算関数
+function calculateProduct() {
+    const num1 = parseFloat(document.getElementById("num1").value);
+    const num2 = parseFloat(document.getElementById("num2").value);
+    const product = num1 * num2;
+    document.getElementById("result").textContent = product;
 }
